9a0e0dac6cf095255f42646cfc4b9bca6e171093
Dauntae <dauntae9@gmail.com>
2024-07-28 00:57:36 -0500

    ConversionScreen Created

--- Calpal/App.js
+++ Calpal/App.js
@@ -2,12 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ConversionScreen from "./app/screens/ConversionScreen";
 
 export default function App() {
   // Remember to delete this console.log
   console.log("App Executed");
 
-  return <WelcomeScreen />;
+  return <ConversionScreen />;
 }
 
 const styles = StyleSheet.create({
--- Calpal/app/screens/ConversionScreen.js
+++ Calpal/app/screens/ConversionScreen.js
@@ -1,16 +1,64 @@
 import React from "react";
-import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+} from "react-native";
+
+import colors from "../config/colors";
 
 function ConversionScreen(props) {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState("");
+
   return (
-    <div>
-      <Text>Hello Calculator</Text>
-    </div>
+    <ImageBackground
+      resizeMode="container"
+      style={styles.background}
+      source={require("../assets/Conversionbg.jpg")}
+    >
+      <Text>Enter A Value</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+
+      <View style={styles.convertButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  pass,
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  convert: {
+    backgroundColor: "#000",
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 export default ConversionScreen;
